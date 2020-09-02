affa2566138778cf70ad565e3315806a8a632381
Nikita <nikita.myslivets@mail.ru>
2020-09-02 16:59:13 +0300

    connect css

--- server.py
+++ server.py
@@ -1,24 +1,41 @@
-#import traceback
+import traceback
 from http.server import SimpleHTTPRequestHandler
 
 import settings
 #from errors import MethodNotAllowed
-#from errors import NotFound
-#from utils import normalize_path
-#from utils import read_static
-#from utils import to_bytes
+from errors import NotFound
+from utils import normalize_path
+from utils import read_static
+from utils import to_bytes
+from utils import get_name_from_qs
+#from custom_types import Endpoint
 
 
 class MyHttp(SimpleHTTPRequestHandler):
     def do_GET(self):
-        path = self.build_path()
+        path = normalize_path(self.path)
+      #  endpoint = Endpoint.from_path(self.path)
+        handlers = {
+            "/": self.handle_root,
+            "/hello/": self.handle_hello,
+            "/style/": self.handle_style, 
+         #   "/img/": self.handle_logo, 
+        }
 
-        if path == "/":
-            self.handle_root()
-        elif path == "/hello/":
-            self.handle_hello()
-        else:
+        try:
+            handler = handlers[path]
+            handler()
+        except(NotFound, KeyError):
             self.handle_404()
+
+        # if path == "/":
+        #     self.handle_root()
+        # elif path == "/hello/":
+        #     self.handle_hello()
+        # elif path == "/style/":
+        #     self.handle_style()    
+        # else:
+        #     self.handle_404()
 
 
     def handle_root(self):
@@ -27,10 +44,26 @@ class MyHttp(SimpleHTTPRequestHandler):
     def handle_hello(self):
         content = f"""
         <html>
-        <head><title>Hello Page</title></head>
+        <head>
+            <title>Hello Page</title>
+            <link rel="stylesheet" type="text/css" href="/style">
+        </head>
         <body>
+
         <h1>Hello world!</h1>
         <p>path: {self.path}</p>
+        
+        <form>
+            <label for="xxx-id">Your name:</label>
+            <input type="text" name="xxx" id="xxx-id">
+            <button type="submit">OK</button>
+            
+            <label for="xxx-id">Your age:</label>
+            <input type="<text></text>" name="yyy" id="yyy-id">
+            <button type="submit">OK</button>
+                
+        </form>
+
         </body>
         </html>
         """
@@ -41,24 +74,29 @@ class MyHttp(SimpleHTTPRequestHandler):
 
     def handle_404(self):
         msg = """NOT FOUND!!!!!!!!"""
-
         self.respond(msg, code=404, content_type="text/plain")
 
+    def handle_style(self):
+        css = read_static("styles/style.css")
+        self.respond(css, content_type="text/css")    
+
+
+    # def handle_logo(self):
+    #     image = read_static("img/cloud.png")
+    #     self.respond(image, content_type="image/png")        
+
+
     def respond(self, message, code=200, content_type="text/html"):
+        payload = to_bytes(message)
+
         self.send_response(code)
         self.send_header("Content-type", content_type)
-        self.send_header("Content-length", str(len(message)))
+        self.send_header("Content-length", str(len(payload)))
         self.send_header("Cache-control", f"max-age={settings.CACHE_AGE}")
         self.end_headers()
-        self.wfile.write(message.encode())
+        self.wfile.write(payload)
 
 
 
-    def build_path(self) -> str:
-        result = self.path
 
-        if result[-1] != "/":
-            result = f"{result}/"
-
-        return result    
 
--- utils.py
+++ utils.py
@@ -3,27 +3,28 @@ from errors import NotFound
 
 
 def normalize_path(path: str) -> str:
-	if not path:
-		return '/'
+    if not path:
+        return "/"
 
-		normalize_path = path
+    normalized_path = path
 
-		if normalize_path[-1] != '/':
-			normalize_path = f'{normalize_path}/'
+    if normalized_path[-1] != "/":
+        normalized_path = f"{normalized_path}/"
 
-		return normalize_path	
+    return normalized_path
 
 
 def to_bytes(text) -> bytes:
-	if isinstance(text, bytes):
-		return text
+    if isinstance(text, bytes):
+        return text
 
-	if not isinstance(text, str):
-		msg = f'cannot convert {type(text)} to bytes'
-		raise ValueError(msg)
+    if not isinstance(text, str):
+        msg = f"cannot convert {type(text)} to bytes"
+        raise ValueError(msg)
 
-	result = text.encode()
-	return result	
+    result = text.encode()
+    return result	
+
 
 def read_static(path: str) -> bytes:
     static = settings.STATIC_DIR / path
@@ -35,4 +36,20 @@ def read_static(path: str) -> bytes:
     with static.open("rb") as fp:
         result = fp.read()
 
-    return result				
+    return result		
+
+
+def get_name_from_qs(qs: str) -> str:
+	if not qs:
+		return 'world'
+
+	pairs = qs.split("&")
+	
+	for pair in pairs:
+		if "=" not in pair:
+			continue
+
+		key, value = pair.split('=')
+		if key == 'xxx':
+			return value
+	return 'world'					    		
